11f3eff2b122be7a604fdc1c84ca34a11af85d34
Richard Page-Croft <rpcroft@gmail.com>
2017-02-03 16:39:24 +0000

    adding a new pet to hash, 13th test

--- pet_shop_spec.rb
+++ pet_shop_spec.rb
@@ -144,11 +144,11 @@ class TestPetShop < Minitest::Test
     assert_equal(nil, pet)
   end
 
-  # def test_add_pet_to_stock
-  #   add_pet_to_stock(@pet_shop, @new_pet)
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(7, count)
-  # end
+  def test_add_pet_to_stock
+    add_pet_to_stock(@pet_shop, @new_pet)
+    count = stock_count(@pet_shop)
+    assert_equal(7, count)
+  end
 
   # def test_customer_pet_count
   #   count = customer_pet_count(@customers[0])
