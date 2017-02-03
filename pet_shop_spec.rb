60d3db6472f6e066e991726db4d37cafaff239f9
Richard Page-Croft <rpcroft@gmail.com>
2017-02-03 16:27:36 +0000

    From an array of hashes within a hash, a hash is deleted by given hask key, 12th test

--- pet_shop_spec.rb
+++ pet_shop_spec.rb
@@ -127,21 +127,22 @@ class TestPetShop < Minitest::Test
     assert_equal("Arthur", pet[:name])
   end
 
-  # def test_find_pet_by_name__returns_nil
-  #   pet = find_pet_by_name(@pet_shop, "Fred")
-  #   assert_equal(nil, pet)
-  # end
-
   def test_find_pet_by_name__returns_nil
     pet = find_pet_by_name(@pet_shop, "Fred")
-    assert_nil(nil, pet)
+    assert_equal(nil, pet)
   end
 
-  # def test_remove_pet_by_name
-  #   remove_pet_by_name(@pet_shop, "Arthur")
-  #   pet = find_pet_by_name(@pet_shop,"Arthur")
-  #   assert_equal(nil, pet)
+# commenting out using assert_nil edit
+  # def test_find_pet_by_name__returns_nil
+  #   pet = find_pet_by_name(@pet_shop, "Fred")
+  #   assert_nil(nil, pet)
   # end
+
+  def test_remove_pet_by_name
+    remove_pet_by_name(@pet_shop, "Arthur")
+    pet = find_pet_by_name(@pet_shop,"Arthur")
+    assert_equal(nil, pet)
+  end
 
   # def test_add_pet_to_stock
   #   add_pet_to_stock(@pet_shop, @new_pet)
